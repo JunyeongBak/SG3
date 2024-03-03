88ca0ee9007654de7cc54df3692a5ddaf164a5b7
JunyeongBak <160200667+JunyeongBak@users.noreply.github.com>
2024-03-03 22:01:50 +0900

    version 0.1.1
    minor patch

--- SG3_tune/docker-entrypoint.sh
+++ SG3_tune/docker-entrypoint.sh
@@ -6,7 +6,7 @@ docker --version
 echo "Python 스크립트 sg3_install.py를 실행합니다."
 python3 sg3_install.py
 
-docker build --tag stylegan3 .
+cd stylegan3 && pwd && docker build --tag stylegan3 ..
 
 # 스크립트가 종료되지 않고 계속 실행되도록 대기 상태 유지
 # 이는 컨테이너가 바로 종료되는 것을 방지합니다.
